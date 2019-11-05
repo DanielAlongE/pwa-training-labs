b5092d7318b5d3e87fa416a2952470fc5ceea339
Daniel Alonge <BrownMarble@outlook.com>
2019-11-05 12:51:43 +0100

    cache-api-lab 04

--- cache-api-lab/app/sw.js
+++ cache-api-lab/app/sw.js
@@ -20,3 +20,34 @@ self.addEventListener('install', event => {
             })
     )
 })
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    console.log('Fetch request ', event.request);
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if(response){
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request)
+                    .then(response => {
+                        return caches.open(staticCacheName)
+                            .then(cache => {
+                                cache.put(event.request.url, response.clone());
+                                return response;
+                            });
+                    })
+          
+                // TODO 4 - Add fetched files to the cache
+          
+            }).catch(error => {
+                      // TODO 6 - Respond with custom offline page
+
+            })
+    );
+});
